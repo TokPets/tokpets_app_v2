3ea4c2565d026da2cfba7d4d0f4165d5a946b521
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-07-25 12:34:50 -0500

    Git Update on deploy/dev

--- src/config/sw/registerServiceWorker.ts
+++ src/config/sw/registerServiceWorker.ts
@@ -33,8 +33,6 @@ if (process.env.NODE_ENV === 'production') {
     },
   });
 
-  self.addEventListener('message', function (event) {
-    console.log("SW Received Message: " + event.data);
-  });
+
 
 }
--- src/config/sw/service-worker.js
+++ src/config/sw/service-worker.js
@@ -17,27 +17,16 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
+/*
 firebase.initializeApp({
     'messagingSenderId': 'YOUR-SENDER-ID'
 });
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
 const messaging = firebase.messaging();
-
-
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.setBackgroundMessageHandler` handler.
 messaging.onMessage(function (payload) {
     console.log('Message received. ', payload);
-    // ...
 });
+*/
 
 self.addEventListener('message', function (event) {
-    console.log("SW Received Message: " + event.data);
+    console.log("SW Received Message: " + JSON.stringify(event.data));
 });
