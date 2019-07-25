2ca859b45561120fb6352e2c68b8163bf833baf9
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-07-24 22:38:35 -0500

    Git Update on master

--- server/deploy/class/AppDeploy.class.js
+++ server/deploy/class/AppDeploy.class.js
@@ -23,11 +23,19 @@ class AppDeploy {
 
         logs.consoleLogTitle(initMessage);
 
-        const isCLS = await exeCommand('cls');
-        const isLBuild = await exeCommand('npm run build');
         const currentBranch = await gitBranch();
+        const currentProject = (this.branchs.filter((branch) => branch.branch === currentBranch)[0] || []);
+        const currentCommitMessage = 'Git Update on ' + currentBranch;
+
+        const isCLS = await exeCommand(`cls`);
+        const isBuilded = await exeCommand(`npm run build`);
+        const isGitAdd = await exeCommand(`git add *`);
+        const isGitCommit = await exeCommand(`git commit -m "${currentCommitMessage}"`);
+        const isChanged = await exeCommand(`firebase use ${currentProject}`);
+        const isDeployed = await exeCommand(`firebase deploy`);
+
         console.log('')
-        console.log('currentBranch ')
+        console.log('currentBranch')
         console.log(currentBranch)
         console.log('')
         //BUILD
