7370d4d9f8312ef35d4a1f7955e4a5b12c03f204
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-07-25 23:21:35 -0500

    Git Update on deploy/dev

---
+++ src/views/login.signin.view/login.signinview.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="view page index">
+    <h1>TOK PETS // LOGIN // SIGNIN </h1>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+import ConfigFirebase from './../../config/firebase/config.firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+import 'firebase/functions';
+import 'firebase/storage';
+
+firebase.initializeApp(ConfigFirebase);
+
+@Component({})
+export default class IndexView extends Vue {
+
+  private CurrentNavigator: string = '';
+
+  private beforeMount() {
+
+  }
+
+  private mounted() {
+
+  }
+
+
+}
+</script>
+
+
+
+<style lang="less">
+</style>
