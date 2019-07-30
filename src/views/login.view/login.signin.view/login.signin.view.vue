6c30e89270c0a7dbfe96007ad39ce1072c4566c3
Ing. Jorge Luis Mayorga Taboda <40527919+IngJorgeLuisMayorga@users.noreply.github.com>
2019-07-29 23:10:56 -0500

    Git Update on deploy/dev

--- src/views/login.view/login.signin.view/login.signin.view.vue
+++ src/views/login.view/login.signin.view/login.signin.view.vue
@@ -12,8 +12,25 @@ import './../../../config/firebase/_index';
 import './../../../config/vndrs/_index';
 import './../../../config/srvcs/_index';
 
+import * as firebase from 'firebase/app';
+const messaging = firebase.messaging();
+
 @Component({})
-export default class IndexView extends Vue {}
+export default class IndexView extends Vue {
+
+  private mounted(){
+
+    Notification.requestPermission().then(function (permission) {
+    messaging.getToken().then((token: any) => {
+        // tslint:disable-next-line: no-console
+        console.log('FCM from PWA :: token');
+        console.log(token);
+    });
+    }).catch((err: any) => {
+    console.log('Unable to get permission to notify.', err);
+    });
+  }
+}
 </script>
 
 
